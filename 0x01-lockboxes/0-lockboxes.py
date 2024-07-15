de032a8e90634ea6c624a7e306ee0e7ad4f2a229
chessboy71 <bentegrimohamed@gmail.com>
2024-07-15 02:03:00 +0200

    s

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,22 +1,17 @@
 #!/usr/bin/python3
+"""Script will unlock list of lists"""
+
+
 def canUnlockAll(boxes):
-    """A function that checks if all the boxes can be opened
-
-    Args:
-        boxes (array of numbers):
-
-    Returns:
-        boolean.
+    """This function will take a list of lists and the content
+       of a list will unlock other lists
     """
-    queue = [0]
-    visited = [False] * len(boxes)
-    while (len(queue) > 0):
-        currentKeyUsed = queue.pop()
-        if visited[currentKeyUsed] == True:
-            continue
-        queue = queue + boxes[currentKeyUsed]
-        visited[currentKeyUsed] = True
-    for box in visited:
-        if not box:
-            return False
-    return True
+
+    keys = [0]
+    for key in keys:
+        for boxKey in boxes[key]:
+            if boxKey not in keys and boxKey < len(boxes):
+                keys.append(boxKey)
+    if len(keys) == len(boxes):
+        return True
+    return False
